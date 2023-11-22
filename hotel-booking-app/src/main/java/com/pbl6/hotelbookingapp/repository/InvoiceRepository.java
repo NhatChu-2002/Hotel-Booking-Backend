aaf6ede9b17b28f27d39df6aeef0c4cdaa239e8b
NhatChu <chunhat321123@gmail.com>
2023-11-22 20:59:37 +0700

    feat: add api cancel

--- hotel-booking-app/src/main/java/com/pbl6/hotelbookingapp/repository/InvoiceRepository.java
+++ hotel-booking-app/src/main/java/com/pbl6/hotelbookingapp/repository/InvoiceRepository.java
@@ -4,5 +4,9 @@ import com.pbl6.hotelbookingapp.entity.Invoice;
 import com.pbl6.hotelbookingapp.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface InvoiceRepository extends JpaRepository<Invoice, Integer> {
+
+    Optional<Invoice> findByReservation(Reservation reservation);
 }
