49d015b4525c02b71fb6f262a8b96d8e14239df7
Vuong Viet Tay <tayvuong46@gmail.com>
2023-12-08 21:44:38 +0700

    change return data

--- hotel-booking-app/src/main/java/com/pbl6/hotelbookingapp/dto/AddRoomTypeResponse.java
+++ hotel-booking-app/src/main/java/com/pbl6/hotelbookingapp/dto/AddRoomTypeResponse.java
@@ -2,14 +2,12 @@ package com.pbl6.hotelbookingapp.dto;
 
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class AddRoomTypeResponse {
-    private String message;
+    private Integer roomTypeId;
 }
