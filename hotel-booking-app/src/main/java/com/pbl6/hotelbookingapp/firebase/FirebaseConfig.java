dd519f556040e468376fcf8b08b1c47ae8116816
NhatChu <chunhat321123@gmail.com>
2023-12-09 11:09:39 +0700

    fix api image

--- hotel-booking-app/src/main/java/com/pbl6/hotelbookingapp/firebase/FirebaseConfig.java
+++ hotel-booking-app/src/main/java/com/pbl6/hotelbookingapp/firebase/FirebaseConfig.java
@@ -1,39 +1,3 @@
-//package com.pbl6.hotelbookingapp.firebase;
-//
-//import com.google.auth.oauth2.GoogleCredentials;
-//import com.google.firebase.FirebaseApp;
-//import com.google.firebase.FirebaseOptions;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.core.io.Resource;
-//
-//import java.io.IOException;
-//import java.io.InputStream;
-//
-//@Configuration
-//public class FirebaseConfig {
-//    @Value("classpath:serviceAccountKey.json")
-//    private Resource resource;
-//
-//    @Bean
-//    public void initFirebase() throws IOException {
-//
-//        InputStream serviceAccount = resource.getInputStream();
-//
-//        FirebaseOptions options = FirebaseOptions.builder()
-//                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-//                .setStorageBucket("hotel-booking-cb28a.appspot.com")
-//                .build();
-//
-//        if (FirebaseApp.getApps().isEmpty()) {
-//            FirebaseApp.initializeApp(options);
-//        }
-//    }
-//}
-//
-//
-//
 package com.pbl6.hotelbookingapp.firebase;
 
 import com.google.auth.oauth2.GoogleCredentials;
@@ -45,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
@@ -53,10 +18,11 @@ public class FirebaseConfig {
 
     @Bean
     public void initFirebase() throws IOException {
-        GoogleCredentials credentials = GoogleCredentials.getApplicationDefault();
+
+        InputStream serviceAccount = resource.getInputStream();
 
         FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(credentials)
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .setStorageBucket("hotel-booking-cb28a.appspot.com")
                 .build();
 
@@ -65,3 +31,5 @@ public class FirebaseConfig {
         }
     }
 }
+
+
